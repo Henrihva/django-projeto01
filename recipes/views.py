2fc748597daf07445dea2457225465abe9029b3a
Henri HVA <henrihva@gmail.com>
2026-03-27 09:36:26 -0400

    Usando o model Recipe real (com QuerySet) na view home

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+# from recipes.models import Recipe  # ou 
+from .models import Recipe
 
 # Create your views here.
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request,'recipes/pages/home.html', context={
         # 'name': 'Henri Vieira',
-        'recipes': [make_recipe() for _ in range(11)],
+#        'recipes': [make_recipe() for _ in range(11)],
+        'recipes': recipes,
+    })
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id
+    ).order_by('-id')
+    return render(request,'recipes/pages/home.html', context={
+        # 'name': 'Henri Vieira',
+#        'recipes': [make_recipe() for _ in range(11)],
+        'recipes': recipes,
     })
 
 def recipe(request, id):
